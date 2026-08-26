e4dcfe6c0e8448b82fa5f59cba789245b11d059c
badeliyan <bahram.adeliyan@gmail.com>
2026-08-26 18:54:43 +0330

    fix: use cors-anywhere proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://6a83901dcb486d243403c18c.mockapi.io',
+//   imgbbApiKey: '4d02c61999d89cd808e2bd64de4840ea', // ← کلید خودت
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'https://corsproxy.io/?https://6a83901dcb486d243403c18c.mockapi.io',
+  apiUrl: 'https://api.allorigins.win/raw?url=https://6a83901dcb486d243403c18c.mockapi.io',
   imgbbApiKey: '4d02c61999d89cd808e2bd64de4840ea',
 };
